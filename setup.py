57648b7e0fc020afc0e5daf3e459bde16d269ca3
Rikko Matsumato <rkikkuriku@outlook.com>
2024-12-10 01:42:26 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+import setuptools
+setup(
+    name='PyMP3Lib',
+    version='1.0',
+    author_email='rkikkuriku@outlook.com',
+    author='RikkoMatsumato',
+    license='GPL-2.0 License',
+    description='My First Library for Init MP3 via Winmm library...',
+    packages=setuptools.find_packages(),
+)
