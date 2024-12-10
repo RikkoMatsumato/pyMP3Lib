f835f8a2cf08b948a2f5c86ff7a1c172a74e2a6f
Rikko Matsumato <rkikkuriku@outlook.com>
2024-12-10 02:16:18 +0200

    Update MP3Lib.py

--- pyMP3Lib/MP3Lib.py
+++ pyMP3Lib/MP3Lib.py
@@ -1,13 +1,12 @@
-class MP3:
-    def PlayMusic_Loop(filename : str):
-        import ctypes
-        winmm_lib = ctypes.cdll.LoadLibrary("Winmm")
-        winmm_lib.mciSendStringW(bytes("play {}.mp3 repeat", "utf-8"), 0, 0, 0)
-    def PlayMusic(filename : str):
-        import ctypes
-        winmm_lib = ctypes.cdll.LoadLibrary("Winmm")
-        winmm_lib.mciSendStringW(bytes("play {}.mp3", "utf-8"), 0, 0, 0)
-    def StopMusic(filename : str):
-        import ctypes
-        winmm_lib = ctypes.cdll.LoadLibrary("Winmm")
-        winmm_lib.mciSendStringW(bytes("stop {}.mp3", "utf-8"), 0, 0, 0)
+def PlayMusic_Loop(filename):
+    import ctypes
+    winmm_lib = ctypes.WinDLL("Winmm.dll")
+    winmm_lib.mciSendStringA(bytes("play {}.mp3 repeat".format(filename), "utf-8"), 0, 0, 0)
+def PlayMusic(filename):
+    import ctypes
+    winmm_lib = ctypes.WinDLL("Winmm.dll")
+    winmm_lib.mciSendStringA(bytes("play {}.mp3".format(filename), "utf-8"), 0, 0, 0)
+def StopMusic(filename):
+    import ctypes
+    winmm_lib = ctypes.WinDLL("Winmm.dll")
+    winmm_lib.mciSendStringA(bytes("stop {}.mp3".format(filename), "utf-8"), 0, 0, 0)
